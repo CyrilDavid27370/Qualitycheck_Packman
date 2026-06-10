83b69264cfc46b2321331fa3e495098d6a3eaaae
Cyril David <cdhinho27370@outlook.fr>
2026-06-10 16:14:51 +0200

    ticket4 : structure de la page dashboard

--- app/src/DataFixtures/DashboardFixtures.php
+++ app/src/DataFixtures/DashboardFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\CategoryType;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,44 @@ class DashboardFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $data = [
+            'Moteur thermique'=> [
+                'Moteur',
+                'Boite de vitesse',
+            ],
+            'GMPE' => [
+                'Moteur',
+                'Stator',
+                'Rotor',
+                'PEB',
+                'Réducteur',
+            ],
+            'E-Motor' => [
+                'Moteur AFM',
+                'Carter C-MOT',
+                'Carter C-FERM',
+                'Stator AFM',
+                'Rotor AFM',
+            ],
+            'Module' => [
+                'Modules GPEC Batteries',
+                'Modules PVAL',
+            ],
+            'Batterie' => [],// pas de types pour l'instant
+        ];
+
+        foreach ($data as $categoryName => $types) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+
+            foreach ($types as $typeName) {
+                $type = new CategoryType();
+                $type->setName($typeName);
+                $type->setCategory($category);
+                $manager->persist($type);
+            }
+        }
 
         $manager->flush();
     }
